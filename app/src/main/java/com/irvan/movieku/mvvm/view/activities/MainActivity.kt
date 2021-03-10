f12d6f5bfb046e67517a35a37a0574d7e97fb835
irvan <irvandy.handoyo@jappri.id>
2021-03-10 09:51:14 +0700

    first commit

---
+++ app/src/main/java/com/irvan/movieku/mvvm/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.irvan.movieku.mvvm.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irvan.movieku.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
